e5191c545edc3fc28a5b45e120f520b1777a875c
unknown <yuriprotasio@hotmail.com>
2017-11-14 18:05:30 -0200

    Updates no menu

--- scripts/others/navbar.js
+++ scripts/others/navbar.js
@@ -1,27 +1,45 @@
 jQuery(window).scroll(function() {
 
-  if($(document).scrollTop() > 100){
+  if($(window).width() > 768){
+    if($(document).scrollTop() > 100){
 
-    $('nav').removeClass('navbar-default');
-    $('nav').addClass('navbar-default2-index');
-    $('#nav1').removeClass('navbar-fixed-top');
-    $('#nav1').addClass('navbar-fixed-top2');
-    $('#ul1').removeClass('navbar-right');
-    $('#ul1').addClass('navbar-right2');
+      $('nav').removeClass('navbar-default');
+      $('nav').addClass('navbar-default2-index');
+      $('#nav1').removeClass('navbar-fixed-top');
+      $('#nav1').addClass('navbar-fixed-top2');
+      $('#ul1').removeClass('navbar-right');
+      $('#ul1').addClass('navbar-right2');
 
-    $('nav').addClass('fadeInDown');
+      $('nav').addClass('fadeInDown');
+    }
+
+    else {
+
+      $('nav').removeClass('navbar-default2-index');
+      $('nav').addClass('navbar-default');
+      $('#nav1').removeClass('navbar-fixed-top2');
+      $('#nav1').addClass('navbar-fixed-top');
+      $('#ul1').addClass('navbar-right');
+      $('#ul1').removeClass('navbar-right2');
+
+      $('nav').removeClass('fadeInDown');
+    }
+
   }
+});
 
-  else {
-
-    $('nav').removeClass('navbar-default2-index');
-    $('nav').addClass('navbar-default');
-    $('#nav1').removeClass('navbar-fixed-top2');
-    $('#nav1').addClass('navbar-fixed-top');
-    $('#ul1').addClass('navbar-right');
-    $('#ul1').removeClass('navbar-right2');
-
-    $('nav').removeClass('fadeInDown');
+jQuery(window).on('resize', function() {
+  if($(window).width() < 768){
+      $('nav').removeClass('navbar-fixed-top');
+      $('nav').removeClass('navbar-fixed-top2');
+      $('nav').removeClass('navbar-default2-index');
+      $('#ul1').removeClass('navbar-right2');
+      $('nav').addClass('navbar-default');
+      $('#ul1').addClass('navbar-right');
   }
-
+});
+jQuery(window).on('load', function() {
+  if($(window).width() < 768){
+      $('nav').removeClass('navbar-fixed-top');
+  }
 });
